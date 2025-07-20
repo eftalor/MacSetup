942afa6e86e3acf9769622a09a04a56dc371d68f
Efi Talor <eftalor@gmail.com>
2025-07-20 20:44:27 +0300

    updated Brewfile, fixed brew_first_run.sh

--- brew_first_run.sh
+++ brew_first_run.sh
@@ -1,8 +1,15 @@
 #/bin/bash
 
-curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh
-brew tap Homebrew/bundle
-brew bundle
+if [ -f "/opt/homebrew/bin/brew" ]; then
+  echo "Brew installed, proceeding to bundle install"
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+  brew tap Homebrew/bundle
+  brew bundle
+
+else
+  echo "Installing Brew! once done *RUN THIS FILE AGAIN* or invoke `brew bundle`
+  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+fi
 
 #fixes:
 sudo chmod -R go-w "$(brew --prefix)/share"
